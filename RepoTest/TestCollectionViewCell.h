4c7dabfcf8b3c1c7521f2bfdb0f9047d077d64f3
William <apple@iMac.local>
2015-03-24 10:25:02 +0800

    12334

---
+++ RepoTest/TestCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TestCollectionViewCell.h
+//  RepoTest
+//
+//  Created by LD on 15/3/24.
+//  Copyright (c) 2015年 baojun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ RepoTest/TestCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  TestCollectionViewCell.m
+//  RepoTest
+//
+//  Created by LD on 15/3/24.
+//  Copyright (c) 2015年 baojun. All rights reserved.
+//
+
+#import "TestCollectionViewCell.h"
+
+@implementation TestCollectionViewCell
+
+@end
